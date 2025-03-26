36e7617ca4b81334b42f13c88029a849bcbfada3
syedajamema <67185922+syedajamema@users.noreply.github.com>
2025-03-27 00:54:46 +0530

    Update testhd.py

--- testhd.py
+++ testhd.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1Ys8d5vD0v_yqPPInC5zfm1gU70FPm9l4
 """
 
+pip install scikit-learn
 import streamlit as st
 import pandas as pd
 import numpy as np
